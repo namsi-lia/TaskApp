608b6102bd39944e1ba6fbe10e9efc33ca750936
namsi-lia <namsilydia7@gmail.com>
2023-05-19 12:27:03 +0300

    Add query function to db helper  and controller

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -6,14 +6,23 @@ class TaskController extends GetxController{
 
   @override
   void onReady(){
+    getTasks();
     super.onReady();
 
   
   }
+
+  var taskList = <Task>[].obs;
     //create method for model
     Future<int> addTask({Task? task}) async {
 
       return await DBHelper.insert(task);
     }
+
+    //get all the data from table
+    void getTasks() async {
+      List<Map<String, dynamic>> tasks =await DBHelper.query();
+      taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
+         }
 
 }
